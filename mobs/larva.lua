5e5f5d738295ba1a51196857d0f6643ccd0d4afe
tenplus1 <kinsellaja@yahoo.com>
2021-01-12 20:42:54 +0000

    update particlespawner

--- mobs/larva.lua
+++ mobs/larva.lua
@@ -62,24 +62,24 @@ mobs:register_mob("nssm:larva", {
 
 			self.object:remove()
 
-			minetest.add_particlespawner(
-				200, --amount
-				0.1, --time
-				{x=pos.x-1, y=pos.y-1, z=pos.z-1}, --minpos
-				{x=pos.x+1, y=pos.y+1, z=pos.z+1}, --maxpos
-				{x=-0, y=-0, z=-0}, --minvel
-				{x=1, y=1, z=1}, --maxvel
-				{x=-0.5,y=5,z=-0.5}, --minacc
-				{x=0.5,y=5,z=0.5}, --maxacc
-				0.1, --minexptime
-				1, --maxexptime
-				3, --minsize
-				4, --maxsize
-				false, --collisiondetection
-				"tnt_smoke.png" --texture
-			)
+			minetest.add_particlespawner({
+				amount = 200,
+				time = 0.1,
+				minpos = {x = pos.x - 1, y = pos.y - 1, z = pos.z - 1},
+				maxpos = {x = pos.x + 1, y = pos.y + 1, z = pos.z + 1},
+				minvel = {x = -0, y = -0, z = -0},
+				maxvel = {x = 1, y = 1, z = 1},
+				minacc = {x = -0.5, y = 5, z = -0.5},
+				maxacc = {x = 0.5, y = 5, z = 0.5},
+				minexptime = 0.1,
+				maxexptime = 1,
+				minsize = 3,
+				maxsize = 4,
+				collisiondetection  =false,
+				texture = "tnt_smoke.png"
+			})
 
-			if math.random(1, 2) == 1 then
+			if math.random(2) == 1 then
 				minetest.add_entity(pos, "nssm:mantis")
 			else
 				minetest.add_entity(pos, "nssm:mantis_beast")
